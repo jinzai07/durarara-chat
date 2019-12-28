27b0a935afa175cbe0b9402ecbb7d9c7a1cd3d76
Shinji Yumikura <shinji.yumikura@ffuf.de>
2019-12-28 19:51:11 +0800

    feat: add basic ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ChatComponent } from './chat/chat.component';
+import { ChatBoxComponent } from './chat/chat-box/chat-box.component';
+import { ChatStartComponent } from './chat/chat-start/chat-start.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent, pathMatch: 'full' },
+  { path: 'chat', component: ChatComponent, children: [
+    { path: '', component: ChatStartComponent, pathMatch: 'full' },
+    { path: ':chatroom', component: ChatBoxComponent }
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { ChatBoxComponent } from './chat/chat-box/chat-box.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ChatStartComponent } from './chat/chat-start/chat-start.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    ChatComponent,
+    ChatBoxComponent,
+    ChatStartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
