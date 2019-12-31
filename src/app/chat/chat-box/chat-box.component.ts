8fd294d22bc5e8f50d8fcaafed2f0c000099142c
Shinji Yumikura <shinji.yumikura@ffuf.de>
2019-12-31 21:18:16 +0800

    feat: add loading icon in loading messages

--- src/app/chat/chat-box/chat-box.component.ts
+++ src/app/chat/chat-box/chat-box.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { ChatService } from 'src/app/services/chat.service';
+import { Message } from 'src/app/models/message.model';
 
 @Component({
   selector: 'app-chat-box',
@@ -11,7 +12,8 @@ import { ChatService } from 'src/app/services/chat.service';
   styleUrls: ['./chat-box.component.css']
 })
 export class ChatBoxComponent implements OnInit {
-  messages$: Observable<any>;
+  isLoading: boolean = true;
+  messages$: Observable<Message[]>;
 
   constructor(private chatService: ChatService,
               private activatedRoute: ActivatedRoute) { }
@@ -21,6 +23,7 @@ export class ChatBoxComponent implements OnInit {
     .subscribe(routeParam => {
       this.chatService.getMessages(routeParam.chatroom);
       this.messages$ = this.chatService.messageCollection.valueChanges();
+      this.messages$.subscribe(() => this.isLoading = false);
     });
   }
 
